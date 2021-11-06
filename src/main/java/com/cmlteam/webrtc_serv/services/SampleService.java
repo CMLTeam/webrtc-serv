9676579ae93a075a3edf6bc0646c4b47d3950901
xonix <xonixx@gmail.com>
2021-11-06 19:34:01 +0200

    server scaffolding from cmltemplate

---
+++ src/main/java/com/cmlteam/webrtc_serv/services/SampleService.java
@@ -0,0 +1,34 @@
+package com.cmlteam.webrtc_serv.services;
+
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class SampleService {
+  private final JdbcTemplate jdbcTemplate;
+  private final MongoTemplate mongoTemplate;
+
+  @Autowired
+  public SampleService(JdbcTemplate jdbcTemplate, MongoTemplate mongoTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+    this.mongoTemplate = mongoTemplate;
+  }
+
+  public String getDbVersion() {
+    return jdbcTemplate.queryForObject("select version();", String.class);
+  }
+  public String getMongoVersion() {
+    return mongoTemplate.executeCommand(new Document("buildInfo",1)).get("version", String.class);
+  }
+
+  @PostConstruct
+  public void test() {
+//    System.out.println(getDbVersion());
+//    System.out.println("MONGO: " + getMongoVersion());
+  }
+}
