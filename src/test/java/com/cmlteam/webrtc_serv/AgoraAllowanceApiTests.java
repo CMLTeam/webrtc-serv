3aea43e4dbd783df4d942b08e9208fe659991250
xonix <xonixx@gmail.com>
2021-11-28 23:41:28 +0200

    user+app API : tests

--- src/test/java/com/cmlteam/webrtc_serv/AgoraAllowanceApiTests.java
+++ src/test/java/com/cmlteam/webrtc_serv/AgoraAllowanceApiTests.java
@@ -1,6 +1,5 @@
 package com.cmlteam.webrtc_serv;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -8,10 +7,17 @@ import org.springframework.http.MediaType;
 import java.util.Set;
 
 import static com.cmlteam.webrtc_serv.util.JsonUtil.json;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AgoraAllowanceApiTests extends ApiTestsBase {
+  private static final String EMAIL = "a@a.com";
+  private static final String APP_ID = "aaa1";
+  private static final String JSON_REQUEST =
+      json().add("email", EMAIL).add("appId", APP_ID).toString();
 
   @BeforeEach
   void cleanDb() {
@@ -20,27 +26,54 @@ public class AgoraAllowanceApiTests extends ApiTestsBase {
 
   @Test
   public void testAllowAccessSuccess() throws Exception {
-    String email = "a@a.com";
-    String appId = "aaa1";
+    // WHEN
     mockMvc
         .perform(
-            post("/agora/allowance")
-                .content(json().add("email", email).add("appId", appId).toString())
-                .contentType(MediaType.APPLICATION_JSON))
+            post("/agora/allowance").content(JSON_REQUEST).contentType(MediaType.APPLICATION_JSON))
+        // THEN
         .andExpect(status().isOk());
 
-    Assertions.assertEquals(
-        Set.of(appId), agoraUserRepository.getFirstByEmail(email).orElseThrow().getAppIds());
+    assertEquals(
+        Set.of(APP_ID), agoraUserRepository.getFirstByEmail(EMAIL).orElseThrow().getAppIds());
   }
 
   @Test
   public void testAllowAccessFailsValidationEmail() throws Exception {
-    checkGivesBadRequest("notAnEmail", "aaa1");
+    checkGivesBadRequest("notAnEmail", APP_ID);
   }
 
   @Test
   public void testAllowAccessFailsValidationBlank() throws Exception {
-    checkGivesBadRequest("a@a.com", "");
+    checkGivesBadRequest(EMAIL, "");
+  }
+
+  @Test
+  public void testRevokeAccessSuccess() throws Exception {
+    // GIVEN
+    agoraUserRepository.save(new AgoraUserPersisted(EMAIL, Set.of(APP_ID)));
+
+    // WHEN
+    mockMvc
+        .perform(
+            delete("/agora/allowance")
+                .content(JSON_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON))
+        // THEN
+        .andExpect(status().isOk());
+
+    assertTrue(agoraUserRepository.getFirstByEmail(EMAIL).orElseThrow().getAppIds().isEmpty());
+  }
+
+  @Test
+  public void testRevokeAccessNotFound() throws Exception {
+    // WHEN
+    mockMvc
+        .perform(
+            delete("/agora/allowance")
+                .content(JSON_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON))
+        // THEN
+        .andExpect(status().isNotFound());
   }
 
   private void checkGivesBadRequest(String email, String appId) throws Exception {
